d0427d525fe6965f6ac894ea1ad3ebb2eeb600ed
Sion Park <rockgoat2@naver.com>
2022-09-22 21:51:28 +0900

    feature add folder

---
+++ src/chr_name_crawler.py
@@ -0,0 +1,80 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+import pandas as pd
+
+import tqdm 
+import time 
+import re
+import pickle
+import os
+
+from selenium.webdriver.common.by import By
+
+## infighter url
+url= 'https://dundam.xyz/damage_ranking?page=1&type=7&job=%E7%9C%9E%20%EC%9B%A8%ED%8E%80%EB%A7%88%EC%8A%A4%ED%84%B0&baseJob=%EA%B7%80%EA%B2%80%EC%82%AC(%EB%82%A8)&weaponType=%EC%A0%84%EC%B2%B4&weaponDetail=%EC%A0%84%EC%B2%B4'
+
+driver = webdriver.Chrome('C:/Chromedriver/chromedriver.exe')
+driver.get(url)
+page_src = BeautifulSoup(driver.page_source)
+
+job_box = page_src.find_all('ul', 'job')
+
+re_base_job = re.compile('[가-힣()]+')
+re_job = re.compile('\n')
+
+base_job_list = []
+for i in range(3):
+    job_line = job_box[i].find_all('label')
+    for j in range(len(job_line)):
+        base_job_list.extend(re_base_job.findall(job_line[j].get_text()))
+
+job_dict = {}
+for i in range(len(base_job_list)):
+    time.sleep(1)
+    job_page = driver.find_element(By.XPATH, '/html/body/section/div[1]/div[1]/div/div[2]/div[2]/ul['+str(i//5 +1)+']/div['+str(i%5 +1)+']')
+    job_page.click()
+    page_src = BeautifulSoup(driver.page_source)
+    job_dict[base_job_list[i]] = [re_job.sub( '',x.get_text()) for x in page_src.find('div', class_ = 'ch_evol tble').find_all('div', class_ = 'rkcbox') if 'none' not in x['style']]
+
+        
+job_url= 'https://dundam.xyz/damage_ranking?page=1&type=7&job='+job+ '&baseJob='+base_job+'&weaponType=%EC%A0%84%EC%B2%B4&weaponDetail=%EC%A0%84%EC%B2%B4'
+
+driver.get(job_url)
+
+chr_dict = {'nick_name':[], 'server' :[]}
+stop_idx = False
+while not stop_idx:
+    for j in range(1, 11): 
+        time.sleep(1)
+
+        page_src = BeautifulSoup(driver.page_source)
+
+        rkt_tr_list = page_src.find_all('div', class_= 'rkt-tr')
+        for l in range(1,11):
+            try:
+                nick_name = rkt_tr_list[l].find('span', class_ = 'nik').get_text()
+                chr_dict['nick_name'].append(nick_name)
+
+                server = rkt_tr_list[l].find('span', class_ = 'svname').get_text()
+                chr_dict['server'].append(server)
+
+            except:
+                print('Stop')
+                stop_idx = True
+
+        ## 다음페이지에 해당하는 버튼 누르기 
+        next_page = driver.find_element_by_xpath('//*[@id="ranking"]/div[1]/div[5]/div[3]/div/ul/li['+ str(2+j) +']')
+        next_page.click()
+        
+        if stop_idx:
+            break
+    #중간 저장 
+    with open('C:/Neople_Anal/DAT/'+base_job+'_'+job+'_info', "wb") as fp: 
+        pickle.dump(chr_dict, fp)
+
+# 최종 저장        
+with open('C:/Neople_Anal/DAT/'+base_job+'_'+job+'_info', "wb") as fp: 
+    pickle.dump(chr_dict, fp)       
---
+++ src/get_date_of_creation.py
@@ -0,0 +1,54 @@
+import json
+import requests
+
+import pandas as pd
+
+import time 
+import re
+import pickle
+import os
+from copy import deepcopy
+
+os.chdir('C:/Neople_Anal/')
+
+import tqdm
+import datetime 
+
+import numpy as np
+
+from utils import get_chracter_creation_date, create_continuous_time
+
+api_key_val = 'ZOwcEhhjZbOmV1zLvNKLhYYBTLZy2JjL'
+
+api_url = 'https://api.neople.co.kr/df/'
+
+end_point = 'servers'
+
+url = api_url + end_point + '?apikey='+api_key_val
+res = requests.get(url)
+server_res = json.loads(res.content)
+
+server_df = pd.DataFrame(server_res['rows'])
+
+server_name2id = dict([[x2,x1] for x1, x2 in server_df.values.tolist()])
+
+
+file_names = os.listdir('DAT/job_info')
+
+len_files = len(file_names)
+for i, file_name in enumerate(file_names):
+    with open('C:/Neople_Anal/DAT/job_info/' +file_name, "rb") as fp: 
+        chr_dict = pickle.load(fp)
+    
+    chr_dict['create_date'] = []
+    chr_dict['adventureName'] = []
+    print('Now : ' + file_name + ' \t'+ str(i) +'/' + str(len_files))
+    for name, server in tqdm.tqdm(zip(chr_dict['nick_name'], chr_dict['server'])):
+        date, adv = get_chracter_creation_date(name, server, server_name2id, api_key_val)
+        chr_dict['create_date'].append(date)
+        chr_dict['adventureName'].append(adv)
+    chr_dict['job'] = [file_name]*len(chr_dict['create_date'][:-5])
+
+    with open('C:/Neople_Anal/DAT/chr_info/' + file_name, "wb") as fp: 
+        pickle.dump(chr_dict, fp)
+    
